5c7796d87bf11b504619b2ea26e700a7db0dcc68
chrisdavies23 <chrisdavies@hotmail.com>
2013-01-29 20:18:55 +0000

    amended files

--- dateSearch.php
+++ dateSearch.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta charset="utf-8">
 <title>Date Search Results</title>
 <link href="twitter-bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet">
 <!-- override some twitter-bootstrap styles -->
@@ -13,79 +14,61 @@
 </head>
 <body>
 <div class="container">
-<div  style="width:800px;text-align:justify;padding:40px;">
+<div  style="width: 800px;text-align: justify;padding: 40px;">
 <?php
-$mydate=$_POST['mydate'];
+$mydate = stripslashes($_POST['mydate']);
 
-/*****************************************************************************************************
-   testing data is coming across
-
-if($mydate==""){
-        echo "yo dude, nothing came over your php or html ain't workin'";
-        }
-        else{
-             echo $mydate." sweet, your date was successfully posted over in ENGLISH date format";
-        }
-
-*****************************************************************************************************/
-
-
-
-//convert from british date to american for database using split command
+//convert from british date to american for database using explode command
 //and switching the year, month and date positions
- $newdate=(list($day, $month, $year) = split('[/.-]', $mydate));
- $splitdate= $year."-".$month."-".$day;
+
+$splitDate = preg_split('~[-:/|.]~', $mydate);
+
+list($day, $month, $year) = $splitDate;
 
 
-
-
-/* follwing checkDateFormat function gleaned from http://roshanbh.com.np/2008/05/date-format-validation-php.html     */
-function checkDateFormat($date)
-{
-  //match the format of the date
-  if (preg_match ("/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/", $date, $parts))
-  {
-    //check weather the date is valid of not
-        if(checkdate($parts[2],$parts[3],$parts[1]))
-          return true;
-        else
-         return false;
-  }
-  else
-    return false;
-
-}
- if(checkDateFormat($splitdate))
- {
-
+$switchDate = $year."-".$month."-".$day;
 
 
 //connect to server and select database
   include 'connect.php';
 
 //bring back the dates
-$get_Date = "select ID, date_format(date,'%d %m %Y')as fmt_time, date as dateSet from dateTest where date='$splitdate' order by date desc";
+$get_Date = "select ID, date_format(date,'%d %m %Y')as fmt_time, date as dateSet from dateTest where date='$switchDate' order by date desc";
 $get_date_result = mysql_query($get_Date) or die(mysql_error());
 if (mysql_num_rows($get_date_result) < 1) {
-   //there are no faults, so say so
+   //there are no dates maching your query, so say so
    echo "<table class=\"table\">
             <tr>
-               <th>SORRY, No recorded dates for your search string</th>
+               <th>
+                   SORRY, No recorded dates for your search string
+               </th>
             </tr>
             <tr>
-               <td> <a href=\"dateForm.php\">go back and try another date</a></td>
+               <td>
+                   <a href=\"dateForm.php\">go back and try another date</a>
+               </td>
             </tr>
-          </table> ";
+         </table> ";
          exit;
 } else {
    //create the display string
-   $bangItOutOnThePage = "<table class=\"table\"><tr><th>Query Successful - here is the date you searched for</th>
-   </tr>
-   <tr>
-       <td>ID</td>
-       <td/>Retrieved as English date</td>
-       <td>Recorded date in MySQL format</td>
-   </tr> ";
+   $bangItOutOnThePage = "<table class=\"table\">
+                             <tr>
+                                 <th>
+                                     Query Successful - here is the date you searched for
+                                 </th>
+                             </tr>
+                             <tr>
+                             <td>
+                                 ID
+                             </td>
+                             <td/>
+                                  Retrieved as English date
+                             </td>
+                             <td>
+                                 Recorded date in MySQL format
+                             </td>
+                         </tr> ";
 
    // search the returned array
    while ($pulledDates = mysql_fetch_array($get_date_result)) {
@@ -102,8 +85,6 @@ if (mysql_num_rows($get_date_result) < 1) {
  print $bangItOutOnThePage;
  echo"  <a class=\"other\" href=\"dateForm.php\">go back and try another date</a>";
 
- }else{
-         echo"<strong>you have to enter a valid date:  <a class=\"other\" href=\"dateForm.php\">now go back and enter a REAL date</a></strong>";}
 
  ?>
  </div>
